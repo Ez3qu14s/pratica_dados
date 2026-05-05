6f3a18cd4f6b0074fb594f47d487fbff3e3fe5da
Ezequias Bernardo <se_oliveira@outlook.com>
2026-05-05 18:40:31 -0300

    feat: transformar moeda para numero

--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,14 @@
 import fetchData from "./fetchData.js";
-
-type transacaoPagamento = "Boleto" | "Cartão de Crédito"
-type transacaoStatus = "Paga" | "Recusada pela operadora de cartão" | "Aguardando pagamento" | "Estornada"
-interface TransacaoAPI {
-  Nome: string,
-  ID: number,
-  Data: string,
-  Status: transacaoStatus,
-  Email: string,
-  ['Valor (R$)']: string,
-  ['Forma de Pagamento']: transacaoPagamento,
-  ['Cliente Novo']: number,
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
-  const data = await fetchData<TransacaoAPI[]>('https://api.origamid.dev/json/transacoes.json')
+  const data = await fetchData<TransacaoAPI[]>('https://api.origamid.dev/json/transacoes.json?')
 
-  if(data) {
-    data.forEach(item => {
-      console.log(item);
-    })
-  }
+  if(!data) return
+  const transacoes = data.map(normalizarTransacao)
+
+  console.log(transacoes);
+  
   
 }
 
